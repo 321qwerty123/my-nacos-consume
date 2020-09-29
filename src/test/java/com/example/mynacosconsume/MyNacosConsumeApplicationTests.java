9e9c17346fa4041c8878af0d080f91e6e39f825c
liuzhihang <liuzhihang@ysten.com>
2020-09-29 17:01:20 +0800

    Initial commit

---
+++ src/test/java/com/example/mynacosconsume/MyNacosConsumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mynacosconsume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNacosConsumeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
